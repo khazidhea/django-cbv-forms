2701ab79ee3576f9f2c6fda45f920694da6982a0
khazidhea <azamat@galimzhanov.com>
2020-08-10 03:37:42 +0600

    Add PostForm.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
+    <PostForm @addPost="addPost"></PostForm>
     <Post v-for="post in posts" :post="post" :key="post.id"></Post>
   </div>
 </template>
 
 <script>
 import Post from '@/components/Post.vue'
+import PostForm from '@/components/PostForm.vue'
 
 export default {
   name: 'App',
   components: {
-    Post
+    Post, PostForm
   },
   data: () => ({
     posts: [
       {text: 'First post', created_at: '10 Aug 2020'},
       {text: 'Second post', created_at: '10 Aug 2020'},
     ]
-  })
+  }),
+  methods: {
+    addPost (post) {
+      this.posts.unshift(post)
+    }
+  }
 }
 </script>
---
+++ frontend/src/components/PostForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="form-group">
+    <label for="id_text">Text</label>
+    <input type="text" name="text" id="id_text" class="form-control"
+        minlength="10" required="required" 
+        v-model="post.text">
+    <button @click="addPost">Submit</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostForm',
+  data: () => ({
+    post: {}
+  }),
+  methods: {
+    addPost () {
+      this.post.created_at = '10 Aug 2020'
+      this.$emit('addPost', this.post)
+    }
+  }
+}
+</script>
